48420ecbe7a97ea6284162261090ba418545c912
KevinPeinado33 <kpeinadoleiva@gmail.com>
2026-07-19 12:26:34 -0500

    feat(config): add nativewind

--- App.tsx
+++ App.tsx
@@ -1,15 +1,6 @@
-import { StyleSheet } from 'react-native';
+import './global.css';
 import LoginScreen from './src/features/login/screens/Login.screen';
 
 export default function App() {
   return <LoginScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/features/login/screens/Login.screen.tsx
+++ src/features/login/screens/Login.screen.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function LoginScreen() {
   return (
     <SafeAreaView edges={['top', 'bottom']}>
-      <Text>LoginScreen</Text>
+      <Text className='text-2xl font-bold text-red-500'>LoginScreen</Text>
       <Button title="Login" onPress={() => console.log('Login button pressed')} />
     </SafeAreaView>
   )
