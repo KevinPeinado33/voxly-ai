a90ad96338b249cc75aa1480a49ae44c1737777e
KevinPeinado33 <kpeinadoleiva@gmail.com>
2026-07-19 18:00:13 -0500

    fix: update pnpm lock to native wind

--- src/features/login/screens/Login.screen.tsx
+++ src/features/login/screens/Login.screen.tsx
@@ -1,11 +1,11 @@
 import { Button, Text, View } from 'react-native';
-import { SafeAreaView, useSafeAreaFrame, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function LoginScreen() {
   const { top } = useSafeAreaInsets()
 
-  return (
-    <SafeAreaView edges={['top', 'bottom']}>
+  return ( 
+    <View style={{paddingTop: top }}>
       <Text className='text-2xl font-bold text-red-500'>LoginScreen</Text>
       <Button title="Login" onPress={() => console.log('Login button pressed')} />
     </View>
