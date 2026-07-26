2824fcb96fcdb5ec0734705cf1bdd8805da5ecde
KevinPeinado33 <kpeinadoleiva@gmail.com>
2026-07-26 18:37:14 -0500

    feat: add custom aboslute path

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import './global.css';
-import LoginScreen from './src/features/auth/screens/Login.screen';
+import LoginScreen from '@/features/auth/screens/Login.screen';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { useFonts } from 'expo-font';
 import { LogOut, Zap } from 'lucide-react-native';
-import { ButtonBase } from './src/shared/components/atoms/Button';
-import { Chip } from './src/shared/components/atoms/Chip';
+import { ButtonBase } from '@/shared/components/atoms/Button';
+import { Chip } from '@/shared/components/atoms/Chip';
 
 export default function App() {
   const [loaded] = useFonts({
--- src/features/auth/services/getUsers.service.ts
+++ src/features/auth/services/getUsers.service.ts
@@ -1,16 +1,14 @@
-import { supabase } from "../../../shared";
+import { supabase } from "@/shared";
 
 import { mapUser } from "../mappers/user.mapper";
 import type { UserModel } from "../models/user.model";
 
 export async function getUsers(): Promise<UserModel[]> {
-  console.log("Fetching users...");
   try {
     const { data, error } = await supabase.from("users").select("*");
 
-    console.log("Fetched users:", data);
-
     if (error) throw error;
+
     return mapUser.toModels(data);
   } catch (error) {
     throw new Error("Error fetching users: " + (error as Error).message);
